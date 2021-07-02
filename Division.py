d114bdd57ed19512155a98c66fb63620aa594732
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-02 17:03:28 +0530

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,9 +1,7 @@
-from __future__ import division
-
 if __name__ == '__main__':
-    a = int(raw_input())
-    b = int(raw_input())
+    a = int(input())
+    b = int(input())
     # Python: Division - Hacker Rank Solution START 
-    print(a//b);
-    print(a/b);
+    print((a//b));
+    print((a/b));
     # Python: Division - Hacker Rank Solution END
