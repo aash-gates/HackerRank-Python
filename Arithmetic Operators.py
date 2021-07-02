2d9a1859184582094f47f5c5dae0da3c39d600a2
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-02 17:01:53 +0530

    Update Arithmetic Operators.py

--- Arithmetic Operators.py
+++ Arithmetic Operators.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    # Arithmetic Operators in Python - Hacker Rank Solution START
+    # Arithmetic Operators in Python 
+    # START
     print((a+b));
     print((a-b));
     print((a*b));
