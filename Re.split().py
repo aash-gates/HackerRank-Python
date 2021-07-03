36f175afedb5488f2c890c25ae9bf1012a4c0426
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-03 23:59:22 +0530

    Update Re.split().py

---
+++ Re.split().py
@@ -0,0 +1,19 @@
+1
+2
+3
+4
+5
+6
+7
+
+	
+
+# Re.split() in python - hacker rank solution
+# Python 3
+# Re.split() in python - hacker rank solution START 
+regex_pattern = r"[.,]+"	# Do not delete 'r'.
+# Re.split() in python - hacker rank solution END
+import re
+print("\n".join(re.split(regex_pattern, input())))
+
+
