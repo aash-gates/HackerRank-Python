be40c1c2bc69fc7c2c00b8c0ff938df52400bd48
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-04 00:05:32 +0530

    Update Group(), Groups() & Groupdict().py

--- Group(), Groups() & Groupdict().py
+++ Group(), Groups() & Groupdict().py
@@ -1,12 +1,6 @@
 import re
-m = re.match(r'(\w+)@(\w+)\.(\w+)','username@hackerrank.com')
-m.group(0)       # The entire match 
-'username@hackerrank.com'
-m.group(1)       # The first parenthesized subgroup.
-'username'
-m.group(2)       # The second parenthesized subgroup.
-'hackerrank'
-m.group(3)       # The third parenthesized subgroup.
-'com'
-m.group(1,2,3)   # Multiple arguments give us a tuple.
-('username', 'hackerrank', 'com')
+m = re.search(r'([A-Za-z0-9])\1+',input())
+if m:
+    print m.group(1)
+else:
+    print -1
