6bc42bfc98583f749d460c462d0ab285ccc31af7
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-03 23:52:58 +0530

    Delete Division.py

--- Division.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    # Python: Division - Hacker Rank Solution START 
-    print((a//b));
-    print((a/b));
-    # Python: Division - Hacker Rank Solution END
