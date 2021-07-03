f0e3d77e0a2832e1060c8fa91f495298e4f4af82
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-03 23:53:31 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Python: Division - Hacker Rank Solution START 
+    print((a//b));
+    print((a/b));
+    # Python: Division - Hacker Rank Solution END
