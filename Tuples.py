3f65f62f67a75a4029243143ae9066aa01e111b8
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 12:34:08 +0530

    Update Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    # Tuples in Python - Hacker Rank Solution START
+    t = tuple(integer_list)
+    print(hash(t));
