f227429db5765e0d546bf710c08bd57dbfd1c0cb
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-07 06:06:49 +0530

    Update String Split and Join.py

--- String Split and Join.py
+++ String Split and Join.py
@@ -1,4 +1,9 @@
 def split_and_join(line):
-    line = line.split(" ")
-    line = "-".join(line)
-    return line
+       line = line.split(" ")
+   line = "-".join(line)
+   return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
