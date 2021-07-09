fa02aaaeeb7f0773dae7150f1ec1f040077b9a07
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 21:21:39 +0530

    Update Text Wrap.py

--- Text Wrap.py
+++ Text Wrap.py
@@ -1,4 +1,12 @@
 import textwrap
-S = input()
-w = eval(input())
-print(textwrap.fill(S,w))
+def wrap(string, max_width):
+    for i in range(0,len(string)+1,max_width):
+        result = string[i:i+max_width]
+        if len(result) == max_width:
+            print(result)
+        else:
+            return(result)
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
