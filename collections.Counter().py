acaa9cddbe48213d15f32d20b14aa1bf21b08f5f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 13:15:01 +0530

    Update collections.Counter().py

--- collections.Counter().py
+++ collections.Counter().py
@@ -1,12 +1,12 @@
 from collections import Counter
-X = input()
-S = Counter(map(int,raw_input().split()))
-N = input()
+X = eval(input())
+S = Counter(list(map(int,input().split())))
+N = eval(input())
 earnings = 0
 for customer in range(N):
-    size, x_i = map(int,raw_input().split())
+    size, x_i = list(map(int,input().split()))
     if size in S and S[size] > 0:
         S[size] -= 1
         earnings += x_i
             
-print earnings 
+print(earnings) 
