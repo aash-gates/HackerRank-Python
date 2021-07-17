2beb94461e244921465d75f538adb4014d5da61c
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-18 00:43:14 +0530

    Update Validating and Parsing Email Addresses.py

--- Validating and Parsing Email Addresses.py
+++ Validating and Parsing Email Addresses.py
@@ -1,3 +1,8 @@
 import re
+from email.utils import *
 for i in range(int(input())):
-    print('YES' if re.search(r"^[789]\d{9}$",input()) else 'NO')
+    email = parseaddr(input())
+    # print email[1]
+    # \w is equivalent to a-zA-Z_
+    if bool(re.search(r'^[a-zA-Z][\w\-\.]*@[A-Za-z]+\.[a-zA-Z]{1,3}$', email[1])):
+        print(formataddr(email))
