5524fa3085c681fd626d191d4960f89e12b32f15
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-18 22:22:21 +0530

    Update Floor, Ceil and Rint.py

--- Floor, Ceil and Rint.py
+++ Floor, Ceil and Rint.py
@@ -1,6 +1,9 @@
 import numpy
 
-my_array = numpy.array(list(map(float, input().split())))
-print(numpy.floor(my_array))
-print(numpy.ceil(my_array))
-print(numpy.rint(my_array))
+numpy.set_printoptions(sign=' ')
+
+a = numpy.array(input().split(),float)
+
+print(numpy.floor(a))
+print(numpy.ceil(a))
+print(numpy.rint(a))
