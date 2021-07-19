c5538fff58fc24034c0956d68a39ccc705daaf9d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-19 13:50:49 +0530

    Update Dot and Cross.py

--- Dot and Cross.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-import numpy
-n=int(raw_input())
-a = numpy.array([raw_input().split() for _ in range(n)],int)
-b = numpy.array([raw_input().split() for _ in range(n)],int)
-m = numpy.dot(a,b)
-print (m)
