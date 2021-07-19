d2a0ac2f602ac95d87317dc705af4f8a4c83436b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-19 13:02:06 +0530

    Update Mean, Var, and Std.py

--- Mean, Var, and Std.py
+++ Mean, Var, and Std.py
@@ -1,9 +1,9 @@
 import numpy
 
-N, M = map(int, raw_input().split())
+N, M = list(map(int, input().split()))
 
-my_array = numpy.array([ map(int, raw_input().split()) for i in range(N) ])
+my_array = numpy.array([ list(map(int, input().split())) for i in range(N) ])
 
-print numpy.mean(my_array, axis = 1)
-print numpy.var(my_array, axis = 0)
-print numpy.std(my_array, axis = None)
+print(numpy.mean(my_array, axis = 1))
+print(numpy.var(my_array, axis = 0))
+print(numpy.std(my_array, axis = None))
