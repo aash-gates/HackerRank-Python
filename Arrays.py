4aeb349d75fb42f6758c407858fe9b6eb965d94d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-21 12:28:50 +0530

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -1,3 +1,9 @@
 import numpy
 
-print(numpy.flipud(numpy.array(input().split(),float)))
+def arrays(arr):
+   #revrser array first, convert to float array with numpy
+   return(numpy.array(arr[::-1], float))
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
