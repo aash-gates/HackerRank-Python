41153045984afe72eeb917d419af1946065d5681
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-23 22:35:45 +0530

    Update HTML Parser - Part 2.py

--- HTML Parser - Part 2.py
+++ HTML Parser - Part 2.py
@@ -1,3 +1,10 @@
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        prefix = '\n' in data and 'Multi-line Comment' or 'Single-line Comment'
+        print('>>> {0}\n{1}'.format(prefix, data))
+    def handle_data(self, data):
+        if data is not '\n':
             print('>>> Data\n{0}'.format(data))
 html = ""
 for i in range(int(input())):
